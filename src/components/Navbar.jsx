c6f595b073adb1479e7fef1a9cb2b28898c99b8f
astarta16 <ngedeshuri2001@gmail.com>
2023-08-24 21:14:12 +0400

    finish burger menu on  mobile and desktop versions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,17 @@ import {TbTruckDelivery} from "react-icons/tb"
 import {MdFavorite, MdHelp} from "react-icons/md"
 import {FaWallet} from "react-icons/fa"
 import {FaUserFriends} from "react-icons/fa"
+import { useState } from "react"
 
 const Navbar = () => {
+    const[nav,setNav] =useState(false)
+
+
+
     return (
         <div className="max-w-[1640] mx-auto flex justify-between items-center p-4"> 
             <div className="flex items-center">
-            <div className="cursor-pointer">
+            <div onClick={()=>setNav(!nav)}    className="cursor-pointer">
                 <AiOutlineMenu size={30}/>
             </div>
             <h1 className="text-2xl sm:text-3xl lg:text-4xl">
@@ -31,10 +36,11 @@ const Navbar = () => {
                     <BsFillCartFill size={20} className="mr-2"/> Cart
                 </button>
 
-            <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+                {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
 
-            <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
-                <AiOutlineClose size={30} className="absolute right-4 top-4 cursor-pointer " />
+            <div className= {nav ? `fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300` : `fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300`} >
+                <AiOutlineClose onClick={()=> setNav(!nav)}
+                size={30} className="absolute right-4 top-4 cursor-pointer " />
                 <h2 className="text-2xl p-4">Best <span className="font-bold">Eats</span></h2>
                 <nav>
                     <ul className="flex flex-col p-4">
